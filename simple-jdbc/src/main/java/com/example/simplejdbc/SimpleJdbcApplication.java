8550fb296e38841883305eeee306a52519eaa8f4
sevenchen <chenzhenlin@exetcar.com>
2020-10-10 14:58:25 +0800

    新增simple-jdbc示例代码

--- simple-jdbc/src/main/java/com/example/simplejdbc/SimpleJdbcApplication.java
+++ simple-jdbc/src/main/java/com/example/simplejdbc/SimpleJdbcApplication.java
@@ -1,13 +1,44 @@
 package com.example.simplejdbc;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.jdbc.core.JdbcTemplate;
 
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+@Slf4j
 @SpringBootApplication
-public class SimpleJdbcApplication {
+public class SimpleJdbcApplication implements CommandLineRunner {
+    @Autowired
+    private DataSource dataSource;
+
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
 
     public static void main(String[] args) {
         SpringApplication.run(SimpleJdbcApplication.class, args);
     }
 
+    @Override
+    public void run(String... args) throws Exception {
+        showConnection();
+        showData();
+    }
+
+    private void showConnection() throws SQLException {
+        log.info(dataSource.toString());
+        Connection conn = dataSource.getConnection();
+        log.info(conn.toString());
+        conn.close();
+    }
+
+    private void showData() {
+        jdbcTemplate.queryForList("SELECT * FROM user")
+                .forEach(row -> log.info(row.toString()));
+    }
 }
